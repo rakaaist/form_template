c9726102064445a9896fad4b91dbc8a7470097a7
rakaaist <rakaaist@gmail.com>
2020-11-17 14:11:30 +0200

    Add validate_login function

--- app/functions/form/validators.php
+++ app/functions/form/validators.php
@@ -22,3 +22,26 @@ function validate_user_unique ($email, &$field) {
     return true;
 }
 
+
+/**
+ * Function checks whether the user has already registered;
+ *
+ * @param $filtered_input
+ * @param $form
+ * @return bool
+ */
+function validate_login($filtered_input, &$form) {
+
+    $db_data = file_to_array(DB_FILE);
+
+    foreach($db_data as $user) {
+        if (($user['email'] === $filtered_input['email'])
+            && ($user['password'] === $filtered_input['password'])){
+            return true;
+        }
+    }
+
+    $form['error'] = 'no user';
+    return false;
+}
+
