d1eccb27beb582a15b70f9b73b87da6003b7581d
rakaaist <rakaaist@gmail.com>
2020-11-18 11:58:13 +0200

    Add message to index.php if is_logged_in and otherwise

--- public_html/index.php
+++ public_html/index.php
@@ -2,27 +2,11 @@
 
 require '../bootloader.php';
 
-/**
- * CLIENT SIDE COOKIES
- *
- * $_COOKIE masyvas
- * setcookie request is sent from browser to server
- */
-$user_id = $_COOKIE['user_id'] ?? uniqid();
-$visits = ($_COOKIE['visits'] ?? 0) + 1;
-
-setcookie('user_id', $user_id, time() + 3600);
-setcookie('visits', $visits, time() + 3600);
-
-$h1 = "Hi, this is your ID $user_id";
-$h2 = "You visited $visits times";
-
-
-/**
- * SERVER SIDE COOKIES
- */
-session_start();
-var_dump($_COOKIE);
+if (is_logged_in()) {
+    $h2 = "Welcome {$_SESSION['email']}! Successful login!";
+} else {
+    $h2 = 'You are not logged in.';
+}
 
 ?>
 <!doctype html>
@@ -33,12 +17,6 @@ var_dump($_COOKIE);
     <link rel="stylesheet" href="media/style.css">
 </head>
 <body class="index_body">
-<h1>LABAS, PENKTADIENI!</h1>
-<p><?php print $h1; ?></p>
 <p><?php print $h2; ?></p>
-<?php //require ROOT . '/core/templates/form.tpl.php'; ?>
-<?php //if (isset($error)): ?>
-<!--        <p>--><?php //print $error; ?><!--</p>-->
-<?php //endif; ?>
 </body>
 </html>
