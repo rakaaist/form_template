592609c17f145e143c39e1fc23f512ad5c2c913f
rakaaist <rakaaist@gmail.com>
2020-11-24 14:13:20 +0200

    Object oriented programming - truncateTable(), dropTable(), insertRow(), rowExists(), updateRow()

--- core/classes/FileDB.php
+++ core/classes/FileDB.php
@@ -57,9 +57,9 @@ class FileDB
     }
 
     /**
-     * Function loads data from the file provided;
+     * Function loads data from the file provided
      *
-     * @return array|mixed
+     * @return bool
      */
     public function load(): bool
     {
@@ -87,7 +87,8 @@ class FileDB
     public function createTable(string $table_name): bool
     {
         if (!$this->tableExists($table_name)) {
-            $this->getData()[$table_name] = [];
+            $this->data[$table_name] = [];
+
             return true;
         }
 
@@ -100,7 +101,8 @@ class FileDB
      * @param $table_name
      * @return bool
      */
-    public function tableExists(string $table_name): bool {
+    public function tableExists(string $table_name): bool
+    {
         if (isset($this->getData()[$table_name])) {
             return true;
         }
@@ -108,4 +110,92 @@ class FileDB
         return false;
     }
 
+    /**
+     * Function deletes an array with the table_name index
+     *
+     * @param string $table_name
+     * @return bool
+     */
+    public function dropTable(string $table_name): bool
+    {
+        if ($this->tableExists($table_name)) {
+            unset($this->data[$table_name]);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Function clears table data not the table_name index
+     *
+     * @param string $table_name
+     * @return bool
+     */
+    public function truncateTable(string $table_name): bool
+    {
+        if ($this->tableExists($table_name)) {
+            $this->data[$table_name] = [];
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Function checks whether the row_id exists; if not, creates it with automatic index if not given
+     *
+     * @param string $table_name
+     * @param array $row
+     * @param null $row_id
+     * @return bool|false|int|string
+     */
+    public function insertRow(string $table_name, array $row, $row_id = null)
+    {
+        if (!$this->rowExists($table_name, $row_id)) {
+
+            if ($row_id == null) {
+                $this->data[$table_name][] = $row;
+                $row_id = array_key_last($this->data[$table_name]);
+            } else {
+                $this->data[$table_name][$row_id] = $row;
+            }
+
+            return $row_id;
+        }
+
+        return false;
+    }
+
+    /**
+     * Function checks whether row_id exists
+     *
+     * @param string $table_name
+     * @param $row_id
+     * @return bool
+     */
+    public function rowExists(string $table_name, $row_id): bool
+    {
+        return isset($this->getData()[$table_name][$row_id]);
+    }
+
+    /**
+     * Function rewrites the row array to the new provided
+     *
+     * @param string $table_name
+     * @param $row_id
+     * @param $row
+     * @return bool
+     */
+    public function updateRow(string $table_name, $row_id, array $row): bool {
+        if ($this->rowExists($table_name, $row_id)) {
+            $this->data[$table_name][$row_id] = $row;
+
+            return true;
+        }
+
+        return false;
+    }
 }
--- public_html/test.php
+++ public_html/test.php
@@ -7,7 +7,15 @@ $fileDB->setData(['labas']);
 $fileDB->getData();
 $fileDB->save();
 $fileDB->load();
-$fileDB->createTable('labas');
+$fileDB->createTable('aiste');
 $fileDB->tableExists('labas');
+$fileDB->dropTable('labas');
+$fileDB->truncateTable('aiste');
+$fileDB->insertRow('aiste', ['as'], 0);
+$fileDB->insertRow('aiste', ['as'], 3);
+$fileDB->save();
+$fileDB->load();
+$fileDB->updateRow('aiste', 3, ['labas']);
 
-var_dump($fileDB->tableExists('labas'));
+
+var_dump($fileDB);
